ed8b743f1eb5a8422326d9407e9c965c1d4727f1
Daniel Sabanes Bove <daniel.sabanes_bove@roche.com>
2022-11-18 15:48:37 +0100

    complete test

--- tests/testthat/test-stat_quadrant_counts.R
+++ tests/testthat/test-stat_quadrant_counts.R
@@ -21,5 +21,11 @@ test_that("stat_quadrant_counts", {
   p <- ggplot(make_data_tbl(6), aes(x, y)) +
     geom_point() +
     stat_quadrant_counts()
-  result <- layer_data(p, i = 2) # 2nd layer returns the quadrant statistics.
+  result <- layer_data(p, i = 2)[, c("npcx", "npcy", "label")] # 2nd layer returns the quadrant statistics.
+  expected <- data.frame(
+    npcx = c(0.95, 0.05, 0.05, 0.95),
+    npcy = c(0.95, 0.05, 0.95, 0.05),
+    label = c("n=1", "n=3", "n=2", "n=0")
+  )
+  expect_identical(result, expected)
 })
